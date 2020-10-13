74f79310ca4f00540fc4491118f0d8e2542abdc2
Jimmy Huang <jimmy1999.huang@gmail.com>
2020-10-13 14:24:01 -0400

    Restyle header, more flexible media

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,9 @@ export default function Footer() {
         <div className="section" id="contact">
             <div className="footer">
                 <h1 className="sectionHeader">Contact me</h1>
-                <li><i className="far fa-envelope">: jimmy1999.huang@gmail.com</i></li>
-                <li><i className="fas fa-phone-square-alt"></i>: (317) 496-4642</li>
-                <li href="https://www.linkedin.com/in/jimmyhuang99/"><i className="fab fa-linkedin"></i>: https://www.linkedin.com/in/jimmyhuang99/</li>
-                <li href="github.com"><i className="fab fa-github-square"></i></li><i className="fab fa-github-square"></i>
+                <p>: jimmy1999.huang@gmail.com</p>
+                <p>: (317) 496-4642</p>
+                <p>: LinkedIn</p>
             </div>
         </div>
     )
