c534499bd4479cf6ae9e927e4e93b22266418419
Jimmy Huang <jimmy1999.huang@gmail.com>
2020-10-16 22:42:06 -0400

    Delete unused files

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
             <div className="aboutText">
             <p>I'm graduating from <b>Indiana University</b> in 2020 with a degree in <b>Computer Science</b>. I am current working as a <b>software engineer</b> at <b>Northwestern Mutual</b>. My career goals include eventually joining / creating a startup or trying out product/project manager roles.</p>
 
-            <p>My <b>drive</b> is creating an <b>impact</b> in the community around me. I am constantly looking for opportunities to give back to the community and creating a difference for those who are less fortunate.</p>
+            <p>My <b>drive</b> is creating an <b>impact</b> in the community around me. I am constantly looking for opportunities to give back to the community and creating a difference for those who are less fortunate. During my time at Indiana University, I joined <a href="http://aaaiub.com/"><b>Asian American Association</b></a> as the <b>marketing director</b> to help foster a sense of Asian community in a state where Asian-Americans are traditionally underrepresented. </p>
             </div>
 
         </div>
