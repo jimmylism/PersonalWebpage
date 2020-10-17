cda4a361e62af4b0b1ea66c599a5cbfe6fefc164
Jimmy Huang <jimmy1999.huang@gmail.com>
2020-10-17 01:22:16 -0400

    fix minor typo and finish about me

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,19 @@ export default function About() {
         <div className="section" id="about">
             <h2 className="sectionHeader">A little more about me</h2>
             <div className="aboutText">
-            <p>I'm graduating from <b>Indiana University</b> in 2020 with a degree in <b>Computer Science</b>. I am current working as a <b>software engineer</b> at <a href="https://www.northwesternmutual.com/"><b>Northwestern Mutual</b></a>. My career goals include eventually joining / creating a startup or trying out product/project manager roles.</p>
-
-            <p>My <b>drive</b> is creating an <b>impact</b> in the community around me. I am constantly looking for opportunities to give back to the community and creating a difference for those who are less fortunate. During my time at Indiana University, I joined <a href="http://aaaiub.com/"><b>Asian American Association</b></a> as the <b>marketing director</b> to help foster a sense of Asian community in a state where Asian-Americans are traditionally underrepresented. Another way that I try to impact the community is offering mentorship to those looking to computer science in their academics and careers.</p>
+            I'm graduating from <b>Indiana University</b> in 2020 with a degree in <b>Computer Science</b>. I am currently working as a <b>software engineer</b> at <a href="https://www.northwesternmutual.com/"><b>Northwestern Mutual</b></a>. My career goals include eventually joining / creating a startup or trying out product/project manager roles.
+            <br></br>
+            <br></br>
+            <br></br>
+            
+            My <b>drive</b> is creating an <b>impact</b> in the community around me. I am constantly looking for opportunities to give back to the community and creating a difference for those who are less fortunate. Some of the ways that I try to make an impact are:
+            <br></br>
+            <br></br>
+            <li>Joining <a href="http://aaaiub.com/"><b>Asian American Association</b></a> as the <b>marketing director</b> to help foster a sense of Asian community in a state where Asian-Americans are traditionally under-represented</li>
+            <br></br>
+            <li>Offering mentorship to those looking to computer science in their academics and careers</li>
+            <br></br>
+            <li>Leading a team of four developers as a <b>program manager</b> for <a href="https://iustudentgovernment.indiana.edu/">Indiana University Student Government</a> to better assist the student body</li>
             </div>
 
         </div>
