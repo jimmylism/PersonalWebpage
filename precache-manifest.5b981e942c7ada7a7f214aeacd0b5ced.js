8f7cf80ee20c7cce30e7fb9b83ba515973a8287f
Jimmy Huang <jimmy1999.huang@gmail.com>
2021-06-08 13:20:24 -0400

    Updates

---
+++ precache-manifest.5b981e942c7ada7a7f214aeacd0b5ced.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f961525a50001fddddca795092bf3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fda4754554f5c398bbd",
+    "url": "/static/css/main.0ccb4a88.chunk.css"
+  },
+  {
+    "revision": "e438eadbd715ca27df68",
+    "url": "/static/js/2.0716fa2a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0716fa2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fda4754554f5c398bbd",
+    "url": "/static/js/main.05930b71.chunk.js"
+  },
+  {
+    "revision": "7fd8f8b23b79231f3b83",
+    "url": "/static/js/runtime-main.933364b2.js"
+  },
+  {
+    "revision": "d35b9864eb2d3e811a0d276c6c484ed2",
+    "url": "/static/media/logo.d35b9864.png"
+  }
+]);
